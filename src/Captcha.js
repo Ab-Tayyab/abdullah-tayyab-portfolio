67886039fce6d6b2212850717d063b646eb4f808
abdullahtayyab894 <abdullahtayyab894@gmail.com>
2023-07-29 23:57:31 +0500

    clean

--- src/Captcha.js
+++ src/Captcha.js
@@ -1,14 +1,13 @@
+
 import React, { useEffect, useState } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './component/navbar/Navbar';
 import Home from './component/home/Home'
 import About from './component/about/About';
-import Footer from './component/footer/Footer';
 import Skill from './component/skill/Skill';
 import Project from './component/project/Project';
-import Contact from './component/contact/Contact';
 import Faq from './component/faqs/Faq';
-import ScrollToTop from './component/ScrollTop';
+import Contact from './component/contact/Contact';
+import Footer from './component/footer/Footer';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 import './App.css';
@@ -30,40 +29,28 @@ const Captcha = () => {
 
   return (
     <div className="scroll-container">
-      <BrowserRouter>
-        {isLoading ? (
-          <ul className="loader" >
-            <li style={{ '--i': 1 }}>W</li>
-            <li style={{ '--i': 2 }}>E</li>
-            <li style={{ '--i': 3 }}>L</li>
-            <li style={{ '--i': 4 }}>C</li>
-            <li style={{ '--i': 5 }}>O</li>
-            <li style={{ '--i': 6 }}>M</li>
-            <li style={{ '--i': 7 }}>E</li>
-          </ul>
-        ) : (
-          <>
-            <ScrollToTop />
-            <Navbar />
-            <Routes>
-              <Route
-                path="/"
-                element={
-                  <>
-                    <Home />
-                    <About />
-                    <Skill />
-                    <Project />
-                    <Faq />
-                    <Contact />
-                  </>
-                }
-              />
-            </Routes>
-            <Footer />
-          </>
-        )}
-      </BrowserRouter>
+      {isLoading ? (
+        <ul className="loader" >
+          <li style={{ '--i': 1 }}>W</li>
+          <li style={{ '--i': 2 }}>E</li>
+          <li style={{ '--i': 3 }}>L</li>
+          <li style={{ '--i': 4 }}>C</li>
+          <li style={{ '--i': 5 }}>O</li>
+          <li style={{ '--i': 6 }}>M</li>
+          <li style={{ '--i': 7 }}>E</li>
+        </ul>
+      ) : (
+        <>
+          <Navbar />
+          <Home />
+          <About />
+          <Skill />
+          <Project />
+          <Faq />
+          <Contact />
+          <Footer />
+        </>
+      )}
     </div>
   );
 }
