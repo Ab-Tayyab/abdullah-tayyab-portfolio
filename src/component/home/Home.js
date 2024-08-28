5ae947ffff99fca471cf1f4c7a1c1a56c2cfa774
abdullahtayyab894 <abdullahtayyab414@gmail.com>
2024-08-29 01:49:13 +0500

    user form project added

--- src/component/home/Home.js
+++ src/component/home/Home.js
@@ -1,77 +1,123 @@
-import React from 'react'
-import photo from '../../images/photo3.png'
-import PhoneInTalkIcon from '@mui/icons-material/PhoneInTalk';
-import './Home.css'
+import React from "react";
+import photo from "../../images/photo3.png";
+import PhoneInTalkIcon from "@mui/icons-material/PhoneInTalk";
+import "./Home.css";
 
-import FacebookOutlinedIcon from '@mui/icons-material/FacebookOutlined';
-import GitHubIcon from '@mui/icons-material/GitHub';
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import WhatsAppIcon from '@mui/icons-material/WhatsApp';
+import FacebookOutlinedIcon from "@mui/icons-material/FacebookOutlined";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import WhatsAppIcon from "@mui/icons-material/WhatsApp";
 const Home = () => {
-    return (
-        <div className="home-parent" id="home" >
-            <div className="home-parent-child1">
-                <div className="home-parent-child1-subchild">
-                    <div className="home-parent-child1-subchild1">
-                        <img src={photo} alt="Background"
-                            data-aos="fade-down" data-aos-easing="linear" data-aos-duration="3000"
-                        />
-                    </div>
-                </div>
-            </div>
-            <div>
-                <div className="home-parent-child2">
-                    <div className="home-parent-child2-subchild" >
-                        <h3
-                            data-aos="fade-up" data-aos-duration="500"
-                        >I'm Abdullah Tayyab</h3>
-                        <p
-                            data-aos="fade-up" data-aos-duration="1500"
-                        >React Js Developer</p>
-                        <p
-                            data-aos="fade-up" data-aos-duration="3000"
-                        >I am confident in my ability to create visually appealing and user-friendly interfaces. Although I have also experience with back-end.</p>
-                    </div>
-                    <div>
-                        <div className="home-parent-child2-subchild2">
-                            <a className='social' href="https://www.facebook.com/this.abdullah.8/" target="_blank" rel="noopener noreferrer"
-                                data-aos="zoom-in-up" data-aos-easing="linear" data-aos-duration="500"
-                            >
-                                <FacebookOutlinedIcon />
-                            </a>
-                            <a className='social' href="https://github.com/AbdullahTayyab894" target="_blank" rel="noopener noreferrer"
-                                data-aos="zoom-in-up" data-aos-easing="linear" data-aos-duration="1000"
-                            >
-                                <GitHubIcon />
-                            </a>
-                            <a className='social' href="https://www.linkedin.com/in/abdullahtayyab894/" target="_blank" rel="noopener noreferrer"
-                                data-aos="zoom-in-up" data-aos-easing="linear" data-aos-duration="1500"
-                            >
-                                <LinkedInIcon />
-                            </a>
-                            <a className='social' href="https://wa.me/+923184579618" target="_blank" rel="noopener noreferrer"
-                                data-aos="zoom-in-up" data-aos-easing="linear" data-aos-duration="2000"
-                            >
-                                <WhatsAppIcon />
-                            </a>
-                        </div>
-                    </div>
-                </div>
-                <div className='home-contact'
-                    data-aos="fade-up" data-aos-duration="3000"
-                >
-                    <a className='social-link social-icone' href="https://wa.me/+923085630574" target="_black">
-                        Say Hello!
-                        <PhoneInTalkIcon className="contact-icone" /></a>
-                </div>
-            </div>
-            <div className="slide-container">
-                <div className="slide-text">
-                    <p>Welcome! I'm Abdullah Tayyab, a creative, confident reactjs Developer that have an 6 month of experience in HTML, CSS, JavaScript, ReactJS, Redux, Npm Libraries, MUI, BootStrap, GitHub and also have experience in NodeJS with ExpressJS and MongoDB. I complete my internship which is consist of about 5 to 6 month and complete many projects and after that i attend bootcamp for 2 month and work on both fron-end and back-end and build a Mern E-commerce Site.</p>
-                </div>
-            </div>
+  return (
+    <div className="home-parent" id="home">
+      <div className="home-parent-child1">
+        <div className="home-parent-child1-subchild">
+          <div className="home-parent-child1-subchild1">
+            <img
+              src={photo}
+              alt="Background"
+              data-aos="fade-down"
+              data-aos-easing="linear"
+              data-aos-duration="3000"
+            />
+          </div>
         </div>
-    )
-}
+      </div>
+      <div>
+        <div className="home-parent-child2">
+          <div className="home-parent-child2-subchild">
+            <h3 data-aos="fade-up" data-aos-duration="500">
+              I'm Abdullah Tayyab
+            </h3>
+            <p data-aos="fade-up" data-aos-duration="1500">
+              React Js Developer
+            </p>
+            <p data-aos="fade-up" data-aos-duration="3000">
+              I am confident in my ability to create visually appealing and
+              user-friendly interfaces. Although I have also experience with
+              back-end.
+            </p>
+          </div>
+          <div>
+            <div className="home-parent-child2-subchild2">
+              <a
+                className="social"
+                href="https://www.facebook.com/this.abdullah.8/"
+                target="_blank"
+                rel="noopener noreferrer"
+                data-aos="zoom-in-up"
+                data-aos-easing="linear"
+                data-aos-duration="500"
+              >
+                <FacebookOutlinedIcon />
+              </a>
+              <a
+                className="social"
+                href="https://github.com/AbdullahTayyab894"
+                target="_blank"
+                rel="noopener noreferrer"
+                data-aos="zoom-in-up"
+                data-aos-easing="linear"
+                data-aos-duration="1000"
+              >
+                <GitHubIcon />
+              </a>
+              <a
+                className="social"
+                href="https://www.linkedin.com/in/abdullahtayyab894/"
+                target="_blank"
+                rel="noopener noreferrer"
+                data-aos="zoom-in-up"
+                data-aos-easing="linear"
+                data-aos-duration="1500"
+              >
+                <LinkedInIcon />
+              </a>
+              <a
+                className="social"
+                href="https://wa.me/+923184579618"
+                target="_blank"
+                rel="noopener noreferrer"
+                data-aos="zoom-in-up"
+                data-aos-easing="linear"
+                data-aos-duration="2000"
+              >
+                <WhatsAppIcon />
+              </a>
+            </div>
+          </div>
+        </div>
+        <div
+          className="home-contact"
+          data-aos="fade-up"
+          data-aos-duration="3000"
+        >
+          <a
+            className="social-link social-icone"
+            href="https://wa.me/+923085630574"
+            target="_black"
+          >
+            Say Hello!
+            <PhoneInTalkIcon className="contact-icone" />
+          </a>
+        </div>
+      </div>
+      <div className="slide-container">
+        <div className="slide-text">
+          <p>
+            Welcome! I'm Abdullah Tayyab, a creative, confident reactjs
+            Developer that have an 6 month of experience in HTML, CSS,
+            JavaScript, ReactJS, Redux, Npm Libraries, MUI, BootStrap, GitHub
+            and also have experience in NodeJS with ExpressJS and MongoDB. I
+            complete my internship which is consist of about 5 to 6 month and
+            complete many projects and after that i attend bootcamp for 2 month
+            and work on both fron-end and back-end and build a Mern E-commerce
+            Site.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
--- src/component/project/ProjectAPI.js
+++ src/component/project/ProjectAPI.js
@@ -1,60 +1,74 @@
-import s1 from '../../images/E1.png'
-import s2 from '../../images/i1.png'
-import s4 from '../../images/S1.png'
-import s5 from '../../images/Q1.png'
-import s6 from '../../images/W1.png'
-import s7 from '../../images/C2.png'
-import s8 from '../../images/T1.png'
-
+import s1 from "../../images/E1.png";
+import s2 from "../../images/i1.png";
+import s4 from "../../images/S1.png";
+import s5 from "../../images/Q1.png";
+import s6 from "../../images/W1.png";
+import s7 from "../../images/C2.png";
+import s8 from "../../images/T1.png";
+import s9 from "../../images/L1.png";
 
 export const projectdata = [
-    {
-        id: 1,
-        name: "Coffee Shop",
-        img: s4,
-        url: "https://coffeee-shoop.vercel.app/",
-        detail: "A sleek, user-friendly coffee shop website featuring an intuitive reservation system, responsive design, and a visually appealing layout with seamless navigation, showcasing menu items and services with modern aesthetics."
-    },
-    {
-        id:2,
-        name: "Internee.Pk",
-        img :s2,
-        url: "https://internee-pk-redesign.vercel.app/",
-        detail:"Internship task to redesign Internee.PK website using react."
-    },
-    {
-        id: 2,
-        name: "Expense Tracker App",
-        img: s1,
-        url: "https://reactjs-expense-tracker-app.netlify.app/",
-        detail: "An expense tracker app allows you to monitor and categorize your expenses. Feature Include: Edit"
-    },
-    {
-        id: 3,
-        name: "To Do List",
-        img: s8,
-        url: "https://reactjs-any-do-list.netlify.app/",
-        detail: "A list of errands and other tasks – often written on a piece of paper as a memory aid – that one needs or intends to accomplish."
-    },
-    {
-        id: 4,
-        name: "Quiz App",
-        img: s5,
-        url: "https://reactjs-quiz-app-with-timer.netlify.app/",
-        detail: "A quiz is a form of game or mind sport in which players attempt to answer questions correctly on one or several specific topics."
-    },
-    {
-        id: 5,
-        name: "Time Cunter App",
-        img: s6,
-        url: "https://reactjs-time-counter-app.netlify.app/",
-        detail: " App timer is an incredibly useful tool for time management if you are someone who struggles to get off an app. It can help you save time."
-    },
-    {
-        id: 6,
-        name: "Calculator",
-        img: s7,
-        url: "https://simple-reactjs-calculator.netlify.app/",
-        detail: "Calculator provides simple and advanced mathematical functions in a beautifully designed app. • Perform basic calculations such as addition, subtraction, multiplication, and division."
-    },
-]
+  {
+    id: 1,
+    name: "Coffee Shop",
+    img: s4,
+    url: "https://coffeee-shoop.vercel.app/",
+    detail:
+      "A sleek, user-friendly coffee shop website featuring an intuitive reservation system, responsive design, and a visually appealing layout with seamless navigation, showcasing menu items and services with modern aesthetics.",
+  },
+  {
+    id: 2,
+    name: "Internee.Pk",
+    img: s2,
+    url: "https://internee-pk-redesign.vercel.app/",
+    detail: "Internship task to redesign Internee.PK website using react.",
+  },
+  {
+    id: 3,
+    name: "User Login Signup Form",
+    img: s9,
+    url: "https://userloginform.vercel.app/",
+    detail:
+      "Internship task create signup, login and reset password form using firebase",
+  },
+  {
+    id: 2,
+    name: "Expense Tracker App",
+    img: s1,
+    url: "https://reactjs-expense-tracker-app.netlify.app/",
+    detail:
+      "An expense tracker app allows you to monitor and categorize your expenses. Feature Include: Edit",
+  },
+  {
+    id: 3,
+    name: "To Do List",
+    img: s8,
+    url: "https://reactjs-any-do-list.netlify.app/",
+    detail:
+      "A list of errands and other tasks – often written on a piece of paper as a memory aid – that one needs or intends to accomplish.",
+  },
+  {
+    id: 4,
+    name: "Quiz App",
+    img: s5,
+    url: "https://reactjs-quiz-app-with-timer.netlify.app/",
+    detail:
+      "A quiz is a form of game or mind sport in which players attempt to answer questions correctly on one or several specific topics.",
+  },
+  {
+    id: 5,
+    name: "Time Cunter App",
+    img: s6,
+    url: "https://reactjs-time-counter-app.netlify.app/",
+    detail:
+      " App timer is an incredibly useful tool for time management if you are someone who struggles to get off an app. It can help you save time.",
+  },
+  {
+    id: 6,
+    name: "Calculator",
+    img: s7,
+    url: "https://simple-reactjs-calculator.netlify.app/",
+    detail:
+      "Calculator provides simple and advanced mathematical functions in a beautifully designed app. • Perform basic calculations such as addition, subtraction, multiplication, and division.",
+  },
+];
