0ca45ba9ef62076e384e86e5b753f541ee91d9b8
abdullahtayyab894 <abdullahtayyab414@gmail.com>
2024-09-04 02:39:42 +0500

    about section change

--- src/component/about/About.js
+++ src/component/about/About.js
@@ -1,65 +1,153 @@
-import React from 'react'
-import CountUp from 'react-countup';
-import './About.css'
+import React, { useState } from "react";
+import "./About.css";
+import img from "../../images/photo2.png";
 
-const About = () => {
+function About() {
+  const [activeSection, setActiveSection] = useState("about");
 
-    const onDownloadResume = () => {
-        const link = document.createElement("a");
-        link.href = './abdullah tayyab resume.pdf';
-        link.click();
-    };
-    const onDownloadCoverLetter = () => {
-        const link = document.createElement("a");
-        link.href = './abdullah tayyab cover letter.pdf';
-        link.click();
-    }
-    return (
-        <div id="about">
-            <h1 className='about-heading'>Know Me More</h1>
-            <div className='about-parent' >
-                <div className='about-parent-child1'>
-                    <h1 className='about-name' data-aos="fade-right" data-aos-duration="500">I'm <span>Abdullah Tayyab</span>, a Web Developer</h1>
-                    <p className='detail' data-aos="fade-right" data-aos-duration="1500">I'm a MERN stack developer with six months of experience in front-end web development. I am skilled in HTML, CSS, JavaScript, React, npm libraries, and GitHub. I have worked with front-end frameworks like Bootstrap and MUI to create visually appealing and user-friendly interfaces. I have integrated APIs and third-party libraries into my projects to enhance functionality. Although my back-end development experience is limited, I have a basic understanding of web development principles, including working with databases like MongoDB and server-side languages like Node.js. I am a self-motivated individual with strong communication skills, capable of working both independently and collaboratively with teams and clients. I am dedicated to continuous learning and keeping right with the latest trends and technologies in front-end and back-end web development.</p>
-                </div>
-                <div className='about-parent-child2'>
-                    <div>
-                        <h2 className='about-child2-name' data-aos="fade-right" data-aos-duration="2000"><span>Name:</span>Abdullah Tayyab</h2>
-                        <h2 className='about-child2-email' data-aos="fade-right" data-aos-duration="2000"><span>Email:</span>abdullahtayyab894@gmail.com</h2>
-                        <div className='about-skill' data-aos="fade-right" data-aos-duration="2500">
-                            <div>
-                                <h2 className='about-child2-skill'>  <span>Skills:</span> </h2>
-                            </div>
-                            <div className='about-skill-child'>
-                                <span>HTML</span> <span>CSS</span> <span>JavaScript</span> <span>React JS</span> <span>Npm Libraries</span> <span>MUI</span> <span>BootStrap</span> <span>Git & GitHub</span>
-                            </div>
-                        </div>
-                        <div className='about-experience' data-aos="fade-right" data-aos-duration="3000">
-                            <div>
-                                <h2>
-                                    <CountUp end={6} duration={1} />+
-                                </h2>
-                                <h3>Months of Experiences</h3>
-                            </div>
-                            <div>
-                                <h2>
-                                    <CountUp end={10} duration={1} />+
-                                </h2>
-                                <h3>Projects Completed</h3>
-                            </div>
-                        </div>
-                        <div className='about-btn'>
-                            <button className='about-btn1' onClick={onDownloadResume}>Resume
-                            </button>
-                            <button className='about-btn2' onClick={onDownloadCoverLetter}>Cover Letter
-                            </button>
-                        </div>
-                    </div>
+  const handleToggle = (section) => {
+    setActiveSection(section);
+  };
 
-                </div>
+  const onDownloadResume = () => {
+    const link = document.createElement("a");
+    link.href = "./abdullah tayyab resume.pdf";
+    link.click();
+  };
+  const onDownloadCoverLetter = () => {
+    const link = document.createElement("a");
+    link.href = "./abdullah tayyab cover letter.pdf";
+    link.click();
+  };
+
+  const skills = [
+    { name: "HTML", level: 85 },
+    { name: "CSS", level: 90 },
+    { name: "JavaScript", level: 75 },
+    { name: "React JS", level: 70 },
+    { name: "Npm Libraries", level: 65 },
+    { name: "MUI", level: 80 },
+    { name: "Bootstrap", level: 60 },
+    { name: "Git & GitHub", level: 70 },
+  ];
+
+  return (
+    <div className="about-container">
+      <h1 className="about-heading">Know Me More</h1>
+      <div className="nav">
+        <button
+          onClick={() => handleToggle("about")}
+          className={activeSection === "about" ? "active" : ""}
+        >
+          ABOUT
+        </button>
+        <button
+          onClick={() => handleToggle("expertise")}
+          className={activeSection === "expertise" ? "active" : ""}
+        >
+          EXPERTISE
+        </button>
+      </div>
+
+      <div className={`content ${activeSection}`}>
+        <div
+          className={`section about ${
+            activeSection === "about" ? "show" : "hide"
+          }`}
+        >
+          <div className="about-main">
+            <div>
+              <h1 className="about-name">
+                I'm <span>Abdullah Tayyab</span>, a Web Developer
+              </h1>
+              <p className="detail">
+                I'm a MERN stack developer with six months of experience in
+                front-end web development. I am skilled in HTML, CSS,
+                JavaScript, React, npm libraries, and GitHub. I have worked with
+                front-end frameworks like Bootstrap and MUI to create visually
+                appealing and user-friendly interfaces. I have integrated APIs
+                and third-party libraries into my projects to enhance
+                functionality. Although my back-end development experience is
+                limited, I have a basic understanding of web development
+                principles, including working with databases like MongoDB and
+                server-side languages like Node.js. I am a self-motivated
+                individual with strong communication skills, capable of working
+                both independently and collaboratively with teams and clients. I
+                am dedicated to continuous learning and keeping right with the
+                latest trends and technologies in front-end and back-end web
+                development.
+              </p>
             </div>
+            <div>
+              <img src={img} alt="Personal Photo" className="about-img" />
+            </div>
+          </div>
         </div>
-    )
+        <div
+          className={`section expertise ${
+            activeSection === "expertise" ? "show" : "hide"
+          }`}
+        >
+          <div className="experties-main">
+            <div>
+              <h2 className="about-child2-name">
+                <span>Name:</span>Abdullah Tayyab
+              </h2>
+              <h2 className="about-child2-email">
+                <span>Email:</span>abdullahtayyab894@gmail.com
+              </h2>
+
+              <div className="about-experience">
+                <div>
+                  <h2>6+</h2>
+                  <h3>Months of Experiences</h3>
+                </div>
+                <div>
+                  <h2>10+</h2>
+                  <h3>Projects Completed</h3>
+                </div>
+              </div>
+              <div className="about-btn">
+                <button className="about-btn1" onClick={onDownloadResume}>
+                  Resume
+                </button>
+                <button className="about-btn2" onClick={onDownloadCoverLetter}>
+                  Cover Letter
+                </button>
+              </div>
+            </div>
+            <div>
+              <h2 className="about-child2-skill">
+                <span>Skills:</span>
+              </h2>
+              <div className="skill-bars">
+                {skills.map((skill) => (
+                  <div
+                    className="skill-bar"
+                    key={skill.name}
+                    style={{ maxWidth: "500px" }} // Set maximum width for each bar
+                  >
+                    <div className="skill-info">
+                      <span>{skill.name}</span>
+                      <span>{skill.level}%</span>
+                    </div>
+                    <div className="bar">
+                      <div
+                        className="fill"
+                        style={{
+                          width: `${skill.level}%`, // Correct usage of template literals
+                        }}
+                      ></div>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default About
+export default About;
